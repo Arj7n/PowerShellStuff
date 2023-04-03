a15576761f4f2ab0b4737a53a95b426dd3709ae0
Arj7n <35731470+Arj7n@users.noreply.github.com>
2023-04-02 20:29:13 -0400

    Add files via upload

---
+++ xmlfindandreplace.ps1
@@ -0,0 +1,14 @@
+﻿$targetfolder = "C:\Users\arjun\OneDrive\Documents\MeScript\PS" # where is the folder
+$find = "yo whatsup"   #what part do you want to find 
+$replace ="replace with this" # what do you want to replace it with
+
+cd $targetfolder
+
+
+$folderitems = Get-ChildItem $targetfolder -Filter *.xml |
+ForEach-Object {
+
+(Get-Content $_ -raw) -replace $find,$replace | out-file $_
+
+}
+
